a9b73021a2de6da96898875cd06a6aaab21dea09
novian66 <novian66@gmail.com>
2020-11-10 16:11:20 +0700

    Finish Praktikum 5

--- praktikum5/src/main/java/com/novian_19910027/praktikum5/MainActivity.kt
+++ praktikum5/src/main/java/com/novian_19910027/praktikum5/MainActivity.kt
@@ -1,13 +1,12 @@
-package com.novian_19910027.helloworld
+package com.novian_19910027.praktikum5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener {  }
     }
 }
