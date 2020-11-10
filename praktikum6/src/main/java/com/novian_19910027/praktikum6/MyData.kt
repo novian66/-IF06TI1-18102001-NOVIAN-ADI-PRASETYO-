3ada49a682a4f6fb5580bb5787c85a21e86d0cf1
novian66 <novian66@gmail.com>
2020-11-10 17:39:47 +0700

    item list fix

---
+++ praktikum6/src/main/java/com/novian_19910027/praktikum6/MyData.kt
@@ -0,0 +1,11 @@
+package com.novian_19910027.praktikum6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
